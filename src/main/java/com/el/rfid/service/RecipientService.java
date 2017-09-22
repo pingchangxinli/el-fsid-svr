3f82142c4f63926d1f46ee5de8c7262083d4fbde
spring <spring.zhang@elitesland.com>
2017-09-22 16:37:00 +0800

    收货及其明细接口

--- src/main/java/com/el/rfid/service/RecipientService.java
+++ src/main/java/com/el/rfid/service/RecipientService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface RecipientService {
     /**
-     * 获取待收货订单
+     * 获取JDE待收货订单
      *
      * @param recipientDomain
      * @return
@@ -15,10 +15,26 @@ public interface RecipientService {
     List<RecipientDomain> getUnrecipient(RecipientDomain recipientDomain);
 
     /**
-     * 获取待收货订单明细
+     * 获取JDE待收货订单明细
      *
      * @param recipientDetailDomain
      * @return
      */
     List<RecipientDetailDomain> getUnrecipientDetail(RecipientDetailDomain recipientDetailDomain);
+
+    /**
+     * 将JDE中待收款明细更新到local待收货明细
+     * @param recipientDetailDomain
+     * @return
+     */
+    int recipientJdeToLocal(RecipientDetailDomain recipientDetailDomain);
+
+
+    /**
+     * 获取local待收货订单明细
+     *
+     * @param recipientDetailDomain
+     * @return
+     */
+    List<RecipientDetailDomain> getLocalUnrecipientDetail(RecipientDetailDomain recipientDetailDomain);
 }
--- src/main/java/com/el/rfid/service/RecipientServiceImpl.java
+++ src/main/java/com/el/rfid/service/RecipientServiceImpl.java
@@ -2,7 +2,8 @@ package com.el.rfid.service;
 
 import com.el.rfid.domain.RecipientDetailDomain;
 import com.el.rfid.domain.RecipientDomain;
-import com.el.rfid.mapper.jde.RecipientMapper;
+import com.el.rfid.mapper.jde.JdeRecipientMapper;
+import com.el.rfid.mapper.rfid.RecipientMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,21 +17,41 @@ import java.util.List;
 @Transactional(value = "rfidTransactioinManager")
 public class RecipientServiceImpl implements RecipientService {
     @Autowired
+    private JdeRecipientMapper jdeRecipientMapper;
+    @Autowired
     private RecipientMapper recipientMapper;
     @Autowired
     private RedisTemplate redisTemplate;
 
     @Override
     public List<RecipientDomain> getUnrecipient(RecipientDomain recipientDomain) {
-
-        List<RecipientDomain> list = recipientMapper.getUnrecipient(recipientDomain);
+        List<RecipientDomain> list = jdeRecipientMapper.getUnrecipient(recipientDomain);
         return list;
     }
 
     @Override
     public List<RecipientDetailDomain> getUnrecipientDetail(RecipientDetailDomain recipientDetailDomain) {
+        List<RecipientDetailDomain> list = jdeRecipientMapper.getUnrecipientDetail(recipientDetailDomain);
+        return list;
+    }
 
-        List<RecipientDetailDomain> list = recipientMapper.getUnrecipientDetail(recipientDetailDomain);
+    @Override
+    @Transactional(value = "rfidTransactioinManager")
+    public int recipientJdeToLocal(RecipientDetailDomain recipientDetailDomain) {
+        int result = 0;//默认失败
+        List<RecipientDetailDomain> recipientDetailDomains = jdeRecipientMapper.getUnrecipientDetail(recipientDetailDomain);
+        if (recipientDetailDomains != null && recipientDetailDomains.size() > 0) {
+            recipientMapper.deleteLocalRecipientDetail(recipientDetailDomain);
+            for (int i = 0; i < recipientDetailDomains.size(); i++) {
+                result = recipientMapper.insertLocalRecipientDetail(recipientDetailDomains.get(i));
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<RecipientDetailDomain> getLocalUnrecipientDetail(RecipientDetailDomain recipientDetailDomain) {
+        List<RecipientDetailDomain> list = recipientMapper.getLocalUnrecipientDetail(recipientDetailDomain);
         return list;
     }
 }
